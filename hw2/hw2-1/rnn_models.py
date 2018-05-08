a307ef840c94bcde6d7f8f7f13ae3c812772e9df
Halley <u10116032@gmail.com>
2018-05-08 22:10:36 +0800

    temp update attention

--- hw2/hw2-1/rnn_models.py
+++ hw2/hw2-1/rnn_models.py
@@ -172,8 +172,8 @@ class RnnModel:
         return step
 
 #%%
-'''
-class AttentionalRnnModel:
+
+class RnnModel_Attention:
     def __init__(self, is_training, image_dim, vocab_size, N_hidden, N_video_step, N_caption_step, **params):
 
         self.is_training = is_training
@@ -229,25 +229,26 @@ class AttentionalRnnModel:
         self.word_bias = tf.get_variable('word_bias',
                                   shape= (self.vocab_size),
                                   initializer= tf.constant_initializer())
-        self.z_weight = tf.get_variable('z_weight',
-                                  shape= (self.N_hidden, self.N_hidden),
-                                  initializer= tf.truncated_normal_initializer(stddev= 0.02))
         self.saver = None
+        self.key = tf.get_variable('key',
+                                  shape= (self.vocab_size),
+                                  initializer= tf.truncated_normal_initializer(stddev= 0.02))
+
+    def attention(self, states, key):
+        states_flatten = tf.reshape(states_flatten, (-1, self.N_hidden))
+        states_flatten_normalized = tf.nn.l2_normalize(states_flatten, 1)
+        states_flatten_normalized = tf.matmul(states_flatten_normalized, tf.nn.l2_normalize(self.key))
+        
+        states_flatten_normalized = tf.reshape(
+            states_flatten_normalized, 
+            (self.batch_size, -1, self.N_hidden)
+        )
+        
+        
+        
 
 
-    def attention(self, outputs, key, states):
-        outputs_flatten = tf.reshape(outputs, (-1, self.N_hidden))
-        scores = tf.matmul(tf.matmul(outputs_flatten, self.z_weight), key)
-        scores = tf.nn.softmax(scores)
-        scores = tf.reshape(scores, (self.batch_size, -1))
-        
-        tf_states = tf.transpose(tf.convert_to_tensor(states), [2,0,1,3])
-        c = tf.reduce_sum(tf.multiply(tf_states[:,:,0,:], scores). axis= 1)
-        h = tf.reduce_sum(tf.multiply(tf_states[:,:,1,:], scores). axis= 1)
-        return (c, h)
-        
-        
-    def build_model(self, is_training= True):
+    def build_train_model(self):
         # Inputs
         video = tf.placeholder(dtype=tf.float32,
                 shape=[self.batch_size, self.N_video_step, self.image_dim])
@@ -263,42 +264,21 @@ class AttentionalRnnModel:
         image_embeded = tf.reshape(image_embeded, (-1, self.N_video_step, self.N_hidden))
 
         # RNN parameters
-        state = tf.zeros([self.batch_size, self.encoder_multi_cells])
+        state = self.encoder_multi_cells.zero_state(self.batch_size, dtype= tf.float32)
 
         # Encoding Stage
         with tf.variable_scope('encoder', reuse= tf.get_variable_scope().reuse):
-            encoder_outputs = []
-            encoder_states = []
-            for i in range(self.N_video_step):
-                output, state = tf.nn.dynamic_rnn(self.encoder_multi_cells, image_embeded, initial_state= state)
-                encoder_outputs.append(tf.squeeze(output))
-                encoder_states.append(state)
-            encoder_outputs = tf.stack(encoder_outputs, axis= 1)
-                    
+            encoder_outputs, state = tf.nn.dynamic_rnn(self.encoder_multi_cells, image_embeded, initial_state= state)
+
         # Decoding Stage
         decoder_input_embeded = tf.nn.embedding_lookup(self.word_emdeded, decoder_input)
-        with tf.variable_scope('decoder', reuse= tf.get_variable_scope().reuse):
-            
-            if is_training:
-                decoder_output_list = []
-                for i in range(self.N_caption_step - 1):
-                    each_step_word_embeded = tf.reshape(decoder_input_embeded[:,i,:], (self.batch_size, 1, self.N_hidden))
-                    state = self.attention(encoder_outputs, state, encoder_states)
-                    decoder_output, state = tf.nn.dynamic_rnn(self.decoder_multi_cells, each_step_word_embeded, initial_state= state)
-                    decoder_output_list.append(tf.squeeze(decoder_output))
-                decoder_output_list = tf.stack(decoder_output_list, axis= 1)
-            else:
-                state = self.attention(encoder_outputs, state, encoder_states)
-                decoder_output, decoder_final_state = tf.nn.dynamic_rnn(self.decoder_multi_cells, decoder_input_embeded, initial_state= state)
-                decoder_output_list = decoder_output
-
+        with tf.variable_scope('decoder', reuse= tf.get_variable_scope().reuse):     
+            decoder_output, _ = tf.nn.dynamic_rnn(self.decoder_multi_cells, decoder_input_embeded, initial_state= state)
+                        
         # Project N_hidden into vocab_size
-        decoder_output_flatten = tf.reshape(decoder_output_list, (-1, self.N_hidden))
+        decoder_output_flatten = tf.reshape(decoder_output, (-1, self.N_hidden))
         decoder_logits = tf.matmul(decoder_output_flatten, self.word_weight) + self.word_bias
         decoder_logits = tf.reshape(decoder_logits, (self.batch_size, -1, self.vocab_size))
-
-        # Prediction
-        predict_probs = tf.nn.softmax(decoder_logits)
 
         # Loss
         stepwise_cross_entropy = tf.nn.softmax_cross_entropy_with_logits(
@@ -310,9 +290,55 @@ class AttentionalRnnModel:
         gradients = optimizer.compute_gradients(loss)
         clipped_gradients = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in gradients]
         train_step = optimizer.apply_gradients(clipped_gradients)
-        #train_step = optimizer.minimize(loss)
 
-        return video, decoder_input, decoder_target, loss, train_step, predict_probs
+        return video, decoder_input, decoder_target, loss, train_step
+ 
+    
+    def build_test_model(self, sampling= False):
+        # Inputs
+        video = tf.placeholder(dtype=tf.float32,
+                shape=[self.batch_size, self.N_video_step, self.image_dim])
+
+        decoder_input = tf.placeholder(dtype=tf.int32,
+                shape=[self.batch_size, None])
+
+        # Embeded image_feat size to N_hidden
+        video_flatten = tf.reshape(video, (-1, self.image_dim))
+        image_embeded = tf.matmul(video_flatten, self.image_weight) + self.image_bias
+        image_embeded = tf.reshape(image_embeded, (-1, self.N_video_step, self.N_hidden))
+
+        # RNN parameters
+        state = self.encoder_multi_cells.zero_state(self.batch_size, dtype= tf.float32)
+        captions = []
+        
+        # Encoding Stage
+        with tf.variable_scope('encoder', reuse= tf.get_variable_scope().reuse):
+            encoder_outputs, state = tf.nn.dynamic_rnn(self.encoder_multi_cells, image_embeded, initial_state= state)
+
+        # Decoding Stage
+        decoder_input_embeded = tf.nn.embedding_lookup(self.word_emdeded, decoder_input)
+        for idx in range(self.N_caption_step):
+            with tf.variable_scope('decoder', reuse= tf.get_variable_scope().reuse):     
+                decoder_output, state = tf.nn.dynamic_rnn(self.decoder_multi_cells, decoder_input_embeded, initial_state= state)
+                            
+            # Project N_hidden into vocab_size
+            decoder_output_flatten = tf.reshape(decoder_output, (-1, self.N_hidden))
+            decoder_logits = tf.matmul(decoder_output_flatten, self.word_weight) + self.word_bias
+            decoder_logits = tf.reshape(decoder_logits, (self.batch_size, self.vocab_size))
+            
+            probs = tf.nn.softmax(decoder_logits)
+            if sampling:
+                best_choice = tf.multinomial(tf.log(probs), 1)
+            else:
+                best_choice = tf.expand_dims(tf.argmax(decoder_logits, axis= 1), axis= -1)
+            best_choice = tf.cast(best_choice, dtype= tf.int32)
+            
+            decoder_input_embeded = tf.nn.embedding_lookup(self.word_emdeded, best_choice)
+            captions.append(best_choice)
+            
+        captions = tf.squeeze(tf.stack(captions, axis= 1))
+        return video, decoder_input, captions
+    
 
     def save_model(self, sess, model_file, step):
         if self.saver is None:
@@ -331,4 +357,3 @@ class AttentionalRnnModel:
             step = int( checkpoint.model_checkpoint_path.split("-")[1].split(".")[0])
             self.saver.restore(sess,checkpoint.model_checkpoint_path)
         return step
-'''    
